3ee4513c9d2bdc7dde92c917872cfbf4945927f4
ahdrifai1234 <ahdrifai1234@gmail.com>
2024-09-11 21:47:06 +0700

    tambahkan main-page component
    - tambahkan dir features dan exce 'ng generate component main-page'
    - tambahkan MainPageCompeneon pada app component, segingga akan dipanggil saat routenya kosong

--- mon-app/src/app/app.component.ts
+++ mon-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainPageComponent } from './features/main-page/main-page.component';
 import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule],
+  imports: [RouterOutlet, SharedModule, MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
