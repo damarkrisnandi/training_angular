e6c8ff24e0999373d3a78b60352d956dd7adf044
ahdrifai1234 <ahdrifai1234@gmail.com>
2024-09-11 22:01:52 +0700

    menambahkan route ke MainPageComponent

--- mon-app/src/app/features/main-page/main-page.component.ts
+++ mon-app/src/app/features/main-page/main-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, SharedModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
