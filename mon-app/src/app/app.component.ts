dd3da2a2fdd74df978b102e88eb9acac1007c878
damar <damar@gitlab.lab>
2024-09-12 10:51:06 +0700

    - Menambahkan NavigationBarComponent ke imports di app.component.ts - menggunakan <app-navigation-bar /> pada app.component.html

--- mon-app/src/app/app.component.ts
+++ mon-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from './features/main-page/main-page.component';
 import { SharedModule } from './shared/shared.module';
+import { NavigationBarComponent } from './shared/navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule, MainPageComponent],
+  imports: [RouterOutlet, SharedModule, MainPageComponent, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
